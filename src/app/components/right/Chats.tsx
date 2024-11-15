dbe99154817c57489b51b6c7c585720e3c37905b
Saksham5289 <f20200361@pilani.bits-pilani.ac.in>
2024-11-16 00:10:16 +0530

    asd

--- src/app/components/right/Chats.tsx
+++ src/app/components/right/Chats.tsx
@@ -26,14 +26,11 @@ const Chats = () => {
 
   useEffect(() => {
     const getFriends = async () => {
-      const allFriends = await axios.get(
-        "http://13.232.169.110:3000/api/users/all",
-        {
-          headers: {
-            authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      );
+      const allFriends = await axios.get("/api/users/all", {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
       setAllFriends(allFriends.data);
       console.log(allFriends.data);
     };
