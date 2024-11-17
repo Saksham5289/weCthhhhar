5091d547216174bccda33269f18e771430bbc58e
Saksham5289 <f20200361@pilani.bits-pilani.ac.in>
2024-11-18 00:04:30 +0530

    asd

--- src/app/components/center/MessagesComp.tsx
+++ src/app/components/center/MessagesComp.tsx
@@ -23,12 +23,12 @@ const MessagesComp = () => {
   if (typeof window !== "undefined") {
     myvar = localStorage.getItem("token");
   }
-  const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-  // @ts-expect-error "asdffff"
-  const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+  const SUPABASE_URL = "https://whnhrjgxuhzqwvzmyyrv.supabase.co";
+  const SUPABASE_ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indobmhyamd4dWh6cXd2em15eXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5OTc4NzQsImV4cCI6MjA0NjU3Mzg3NH0.6A1sKTE2gNREj0EKViuy8SGbbTLBY4CJSPdUtaPPvK0";
   useEffect(() => {
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
     const fetchAllMessages = async () => {
       const response = await axios.get(
         `/api/messages?friendId=${state.friend.friendId}`,
