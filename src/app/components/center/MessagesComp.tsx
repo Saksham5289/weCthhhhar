ab88ae0dead4702a8772f7ae718e7c07999fa56a
Saksham5289 <f20200361@pilani.bits-pilani.ac.in>
2024-11-17 22:31:06 +0530

    asd

--- src/app/components/center/MessagesComp.tsx
+++ src/app/components/center/MessagesComp.tsx
@@ -24,8 +24,11 @@ const MessagesComp = () => {
     myvar = localStorage.getItem("token");
   }
   useEffect(() => {
-    const SUPABASE_URL = process.env.SUPABASE_URL || "";
-    const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || "";
+    const SUPABASE_URL =
+      process.env.SUPABASE_URL || "https://whnhrjgxuhzqwvzmyyrv.supabase.co";
+    const SUPABASE_ANON_KEY =
+      process.env.SUPABASE_ANON_KEY ||
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indobmhyamd4dWh6cXd2em15eXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5OTc4NzQsImV4cCI6MjA0NjU3Mzg3NH0.6A1sKTE2gNREj0EKViuy8SGbbTLBY4CJSPdUtaPPvK0";
     const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
     const fetchAllMessages = async () => {
